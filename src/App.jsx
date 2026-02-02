dbf451732087706b0c1729ded06848dd137325b3
Ionut Adrian Cenan <ad.ionutadrian@gmail.com>
2026-02-02 02:03:43 +0200

    Initial commit: Portfolio project foundation

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import FramerComponent from '../framer/wrapper'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className='bg-[rgb(21,_19,_18)] min-h-screen w-full flex flex-col items-center overflow-x-hidden relative'>
+      <Navbar />
+      <div className='w-full max-w-[1440px] flex flex-col items-center'>
+        <FramerComponent.Responsive 
+          locale="en" 
+          variants={{
+            base: "VLsfsnnqq",
+            sm: "VLsfsnnqq",
+            md: "MPyOl2SBg",
+            lg: "tsOoHuHnz",
+            xl: "weKj8KEuk"
+          }}
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import NavBarLinks from '../../framer/nav-bar-links'
+
+export default function Navbar() {
+    return (
+        <div className="relative z-50 w-full flex justify-center pt-8 pb-4">
+            <div className="flex items-center gap-6">
+                <NavBarLinks.Responsive />
+            </div>
+        </div>
+    )
+}
